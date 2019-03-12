0529eb32f7d14a464aa5b9c2d37b2279d69fbdc3
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-03-11 23:17:45 -0700

    adding cors

--- src/server.js
+++ src/server.js
@@ -6,13 +6,19 @@ const PORT = process.env.PORT || 3000;
 /* Middlewares */
 // CORS
 const cors = require('cors');
-const {CLIENT_ORIGIN} = require('./config');
+const {CLIENT_ORIGIN} = require('./config/main.config.js');
 
-app.use(
+/* app.use(
     cors({
         origin: CLIENT_ORIGIN
     })
-);
+); */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+});
 
 app.get('/api/*', (req, res) => {
 res.json({ok: true});
@@ -21,3 +27,4 @@ res.json({ok: true});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = {app};
+
