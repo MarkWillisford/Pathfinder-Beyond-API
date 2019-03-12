0e1f0487ff050e964ab5158dbb866a5fc016b31e
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-03-11 18:49:55 -0700

    adding travis

--- test/test-server.js
+++ test/test-server.js
@@ -7,13 +7,13 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('API', function() {
-
-it('should 200 on GET requests', function() {
-    return chai.request(app)
-    .get('/api/fooooo')
-    .then(function(res) {
-        res.should.have.status(200);
-        res.should.be.json;
+    
+    it('should 200 on GET requests', function() {
+        return chai.request(app)
+        .get('/api/fooooo')
+        .then(function(res) {
+            res.should.have.status(200);
+            res.should.be.json;
+        });
     });
-});
 });
