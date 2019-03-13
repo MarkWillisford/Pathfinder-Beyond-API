2767aca91814ecedc855141848cf9f7378f39c16
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-03-13 14:20:36 -0700

    setup tests

--- test/test-server.js
+++ test/test-server.js
@@ -7,7 +7,7 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('API', function() {    
+describe('tests', function() {    
 	before(function(){
 		return runServer();
 	});
@@ -15,13 +15,20 @@ describe('API', function() {
 	after(function(){
 		return closeServer();
     });
-    
-    it('should 200 on GET requests', function() {
-        return chai.request(app)
-        .get('/api/races')
-        .then(function(res) {
-            res.should.have.status(200);
-            res.should.be.json;
+
+    describe('API calls', function(){
+        // I'll go through and check each endpoint for basic return functionality
+        it('should 200 on GET requests', function() {
+            return chai.request(app)
+            .get('/api/races')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a("array");
+                expect(res.body.length).to.be.at.least(1);
+            });
         });
-    });
+
+    })
 });
+
