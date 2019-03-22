91d003a837d7768f4aac9234b90cf71b6cd5bf76
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-03-22 13:36:42 -0700

    prep for extra eyes

--- src/routers/aasimarHeritages.router.js
+++ src/routers/aasimarHeritages.router.js
@@ -34,15 +34,6 @@ router.route('/')
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
-/*   res.json(AasimarHeritage.find()
-      .then(aasimarHeritages => {
-        console.log(aasimarHeritages);
-        res.json(aasimarHeritages)
-      }))
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'something went terribly wrong' });
-      }); */
   });
 
 module.exports = router;
