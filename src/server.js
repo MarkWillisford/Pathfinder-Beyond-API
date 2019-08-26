4b08c5791e7051a039d9e6f188ed09fd0cb6d3b0
Mark Willisford <MarkWillisford@users.noreply.github.com>
2019-08-26 15:54:45 -0700

    working on CORS error

--- src/server.js
+++ src/server.js
@@ -39,6 +39,7 @@ app.use(morgan('common'));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   /* res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); */
+  // BUG #4 CORS ERROR
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
 
@@ -49,7 +50,7 @@ app.use(function (req, res, next) {
   }
   next();
 });
-app.use(cors());
+//app.use(cors());
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
