67c4a4ca24f18d3e88b764bd969c4ece0272a0a0
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-11-05 19:24:55 -0800

    cors

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.use(morgan('common'));
 // CORS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // ,Authorization
   // BUG #4 CORS ERROR
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
