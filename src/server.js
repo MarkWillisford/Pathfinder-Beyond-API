a9f8845fd4a3002f52fbca5c4dbe6cabb61efa14
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-11-05 18:37:41 -0800

    cors

--- src/server.js
+++ src/server.js
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  //res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
   next();
 });
 app.use(cors());
