b523728726b13aaee81557205bbfbca4972c8ace
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-11-06 20:24:51 -0800

    cors

--- src/server.js
+++ src/server.js
@@ -56,6 +56,8 @@ app.use(morgan('common'));
   next();
 }); */
 app.use(function(req, res, next) {
+  console.log("Within cors catch middleware: ");
+  console.log(res);
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
